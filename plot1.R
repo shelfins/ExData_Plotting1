f65e016783720f250df12f6d31976f1ffbd02f6d
shelfins <shelfins@gmail.com>
2015-09-09 16:57:11 -0400

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,4 @@
+subdata <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt" '),sep=";")
+hist(subdata[,3],col='red',xlab="Global Active Power (kilowatts)", main="Global Active Power")
+dev.copy(png,file="plot1.png")
+dev.off()
